8451182b9d7f0ce27a621fe80f78d947da6c3ca5
DaniJa03 <72860970+DaniJa03@users.noreply.github.com>
2020-11-11 11:56:26 +0100

    Add files via upload

---
+++ Stift.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung
+{  
+        abstract class Stift
+        {
+            public int Strichstarke { get; set; }
+            public ConsoleColor Farbe { get; set; }
+            public Type Text { get; set; }
+        }
+}
