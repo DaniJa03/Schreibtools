a6c879c0ce1821ab0de709981fc5d7307bf91172
DaniJa03 <72860970+DaniJa03@users.noreply.github.com>
2020-11-17 20:41:39 +0100

    Add files via upload

---
+++ Buntstift.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung
+{
+    class Buntstift : Stift
+    {
+        public Buntstift(ConsoleColor Farbe, double Strichstärke) : base(Strichstärke, Farbe, "Buntstift")
+        {
+
+        }
+    }
+}
---
+++ Kugelschreiber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung
+{
+    class Kugelschreiber : Stift
+    {
+        public Kugelschreiber(ConsoleColor Farbe, double Strichstärke) : base(Strichstärke, Farbe, "Kugelschreiber")
+        {
+
+        }
+
+    }
+}
